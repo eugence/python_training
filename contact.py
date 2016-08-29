f810219fc04c36e37369ac219207299e5594b6cc
eugence <userspirit@Mac-mini-admin.mshome.net>
2016-08-29 18:44:32 +0300

    создан вспомогательный класс для контактов

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, companyname, address1, email, birthyear, address2):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.companyname = companyname
+        self.address1 = address1
+        self.email = email
+        self.birthyear = birthyear
+        self.address2 = address2
